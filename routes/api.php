5bdff09e4363956d7cb9a7f7eb311e9a430b0815
mcekic03 <ceka.mihajlo03@gmail.com>
2025-07-08 02:38:37 +0200

    docker

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
